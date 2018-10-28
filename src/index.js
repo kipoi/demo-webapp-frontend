6094cd56081d8c266b9545b098f846e4ebf58d52
Goktug Erce <goktugercegurel@gmail.com>
2018-10-28 20:38:13 +0100

    Redux initialization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+import configureStore from './store';
+
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
@@ -8,6 +12,11 @@ import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App /> , document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
